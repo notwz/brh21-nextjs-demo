b8955f4956157e81501187182c6bf5026a105872
Will Zhang <46132945+notwz@users.noreply.github.com>
2021-09-25 14:04:58 -0400

    delte fa

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-export default function Home() {
-  const Article = ({ title, author, preview }) => (
-    <div>
-      <h3>{title}</h3>
-      <p>By {author}</p>
-    </div>
-  )
-  
-  export default () => (
-    <main>
-      <h1>Articles</h1>
-      <Article title="Hello BIGRED//HACKS!" author="@devanflores" />
-      <Article title="Workshops are cool" author="@devanflores" />
-      <Link href="/shopping">
+const Article = ({ title, author, preview }) => (
+  <div>
+    <h3>{title}</h3>
+    <p>By {author}</p>
+  </div>
+);
+
+export default () => (
+  <main>
+    <h1>Articles</h1>
+    <Article title="Hello BIGRED//HACKS!" author="@devanflores" />
+    <Article title="Workshops are cool" author="@devanflores" />
+    <Link href="/shopping">
       <a>Let’s go shopping</a>
     </Link>
-    </main>
-  );
-}
+  </main>
+);
