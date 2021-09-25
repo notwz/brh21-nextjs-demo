271b74adee0f81b8b0bc4f6c9798e0d08de7ae8a
Will Zhang <46132945+notwz@users.noreply.github.com>
2021-09-25 13:59:22 -0400

    next .js demo

---
+++ pages/shopping.js
@@ -0,0 +1,30 @@
+const items = ["Apples", "Oranges", "Pears", "Strawberries"];
+import React, { useState } from "react";
+
+export default () => {
+  const [items, setItems] = useState(["Apples", "Strawberries"]);
+  const [newItem, setNewItem] = useState("");
+  const changeNewItem = (e) => setNewItem(e.target.value);
+  const addItem = () => {
+    setItems((list) => [...list, newItem]);
+    setNewItem("");
+  };
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <div>
+        <input
+          placeholder="New item"
+          onChange={changeNewItem}
+          value={newItem}
+        />
+        <button onClick={addItem}>Add item</button>
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </main>
+  );
+};
