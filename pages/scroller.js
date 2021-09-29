434f4bf93fd5b1b75b5f0b28fcc4debb52c008f8
Will Zhang <46132945+notwz@users.noreply.github.com>
2021-09-29 10:47:50 -0400

    Create scroller.js

---
+++ pages/scroller.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  Move,
+  MoveIn,
+  MoveOut,
+  Sticky,
+  StickyIn,
+  ZoomIn,
+} from "react-scroll-motion";
+
+export default () => { 
+    
